6325d4d66c343f0d14d146f39879e98a61d6e31d
Javan Jones <93276643+JavatheHutt2014@users.noreply.github.com>
2022-04-19 05:44:04 -0400

    Update and rename discord.keys.js.example to discord.keys.js

---
+++ discord.keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // Webhook has format https://discordapp.com/api/webhooks/965910359473020980/
+    hook_id: '965910359473020980', // First part 
+    hook_token: 'h7r3wdkseaHJcHbF5KpoCdOyDhcB8N-Gv9LnBIBOSSHusizZG5ItFSf_ClX6eGoj68hB', // second part
+
+    // Get this from https://discordapp.com/developers/applications/me
+    // Add an application and create a bot user, paste the token here
+    bot_token: 'OTY1NzcwNzk4OTA2MzUxNjE2.Yl4CJQ.n4Vl1t6LZP7Gj6hnlgUt11AjQRE',
+
+    // The Discord channel you want to connect (no '#')
+    channel_name: 'lets-go'
+};
